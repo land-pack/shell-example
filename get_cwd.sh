7ea0bfd7b5074a09f51873ce16a9810aa9f5c94c
land-pack <landpack@sina.com>
2018-01-24 11:45:19 +0800

    add cwd & assignment

---
+++ get_cwd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+echo "DIR is '$DIR'"
---
+++ test_echo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo  'hello'
+echo "World"
+
+
+# Set variable 
+# https://stackoverflow.com/questions/2268104/command-not-found-error-in-bash-variable-assignment
+
+a="abc"
+echo "'$a'"
+
+
+
