aeec48009fe1d4acda70d4ba1e659b4da967fed3
land-pack <landpack@sina.com>
2018-05-07 18:06:44 +0800

    fixed

--- install_gpu.sh
+++ install_gpu.sh
@@ -13,3 +13,16 @@ sudo apt-get install -y gcc g++ git make
 wget http://us.download.nvidia.com/XFree86/Linux-x86_64/375.20/NVIDIA-Linux-x86_64-375.20.run
 chmod +x NVIDIA-Linux-x86_64-375.20.run
 sudo ./NVIDIA-Linux-x86_64-375.20.run
+
+
+sudo apt-get install -y sudo libltdl-dev
+sudo rm -rf /var/lib/apt/lists/*
+
+wget https://raw.github.com/land-pack/docker-example/master/deb/docker-ce_18.03.0~ce-0~ubuntu_amd64.deb
+sudo apt-get clean
+sudo apt-get update
+sudo dpkg -i docker-ce_18.03.0~ce-0~ubuntu_amd64.deb
+apt-get upgrade -f docker
+
+
+sudo nohup dockerd &
